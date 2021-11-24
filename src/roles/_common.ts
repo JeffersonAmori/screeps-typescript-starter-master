db006d036e72c48699c62fdc06d7ceb5d0a53678
Jefferson Amori da Rocha <jefferson_adr@hotmail.com>
2021-11-24 22:09:49 +0000

    Refactore RoleCommon.getEnergy

--- src/roles/_common.ts
+++ src/roles/_common.ts
@@ -1,17 +1,12 @@
 import { RoleUpgrader } from "./upgrader";
 
 export class RoleCommon {
-
-    private static deleteGetEnergyRelatedMemory(creep: Creep) {
-        delete creep.memory.targetEnergySourceId;
-        delete creep.memory.targetEnergySourceNeedsOnlyOneHarvester;
-    }
+    private static _prohibitedIds: string[] = [];
 
     /** @param {Creep} creep **/
     public static getEnergy(creep: Creep): void {
-
         if (creep.memory.targetEnergySourceId) {
-            let targetEnergySource = null;
+            let targetEnergySource: Resource | Structure | Source | null = null;
             try {
                 targetEnergySource = Game.getObjectById(creep.memory.targetEnergySourceId);
 
@@ -27,103 +22,125 @@ export class RoleCommon {
             finally {
             }
 
+            let ret = undefined;
             if (targetEnergySource instanceof Resource) {
-                if (creep.pickup(targetEnergySource) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targetEnergySource);
-                } else {
-                    RoleCommon.deleteGetEnergyRelatedMemory(creep);
-                }
+                ret = creep.pickup(targetEnergySource);
             }
             else if (targetEnergySource instanceof Structure) {
-                if (creep.withdraw(targetEnergySource, RESOURCE_ENERGY, creep.store.getFreeCapacity()) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targetEnergySource);
-                } else {
-                    RoleCommon.deleteGetEnergyRelatedMemory(creep);
-                }
+                ret = creep.withdraw(targetEnergySource, RESOURCE_ENERGY, creep.store.getFreeCapacity());
             }
             else if (targetEnergySource instanceof Source) {
-                if (creep.harvest(targetEnergySource) == ERR_NOT_IN_RANGE)
-                    creep.moveTo(targetEnergySource);
-
-                if (creep.store.getFreeCapacity() == 0) {
-                    RoleCommon.deleteGetEnergyRelatedMemory(creep);
-                }
+                ret = creep.harvest(targetEnergySource);
             }
-            // else if (targetEnergySource instanceof Tombstone) {
-            //     if (targetEnergySource.creep.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-            //         creep.moveTo(targetEnergySource);
-            //     else {
-            //             delete creep.memory.targetEnergySourceId;
-            //     }
-            // }
-        }
-        else {
-            let prohibitedIds: string[] = [];
-            let creepsSolo = _.filter(creep.room.find(FIND_MY_CREEPS), c => c.memory.targetEnergySourceId && c.memory.targetEnergySourceNeedsOnlyOneHarvester);
-            if (creepsSolo && creepsSolo.length > 0)
-                _.forEach(creepsSolo, c => prohibitedIds.push(c.memory.targetEnergySourceId!));
 
-            let storage: StructureStorage | StructureContainer | null = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return ((structure.structureType == STRUCTURE_STORAGE) && structure.store.getUsedCapacity(RESOURCE_ENERGY) > creep.store.getCapacity());
-                }
-            })
-            if (storage) {
-                creep.memory.targetEnergySourceId = storage.id;
-
-                if (creep.store.getFreeCapacity() >= storage.store.getUsedCapacity()) {
-                    creep.memory.targetEnergySourceNeedsOnlyOneHarvester = true;
-                }
+            if (ret == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targetEnergySource);
             }
             else {
-                // var tombstone = creep.pos.findClosestByPath(FIND_TOMBSTONES);
-                // if (tombstone) {
-                //     creep.memory.targetEnergySourceId = tombstone.id;
-                // }
-                // else {
+                RoleCommon.deleteGetEnergyRelatedMemory(creep);
+            }
 
-                // Find all energies
-                let dropedEnergies = creep.room.find(FIND_DROPPED_RESOURCES);
-                // Find the energies allowed to be picked-up
-                let allowedDropedEnergy = _.filter(dropedEnergies, e => prohibitedIds.indexOf(e.id) == -1);
-                // Find the closest one
-                let closestEnergy = creep.pos.findClosestByPath(allowedDropedEnergy);
-                // If found something...
-                if (closestEnergy && !creep.memory.forceMoveToTargetContainer) {
-                    creep.memory.targetEnergySourceId = closestEnergy.id;
+            if (creep.store.getFreeCapacity() == 0) {
+                RoleCommon.deleteGetEnergyRelatedMemory(creep);
+            }
+        }
+        else {
+            let creepsWorkingSolo = _.filter(creep.room.find(FIND_MY_CREEPS), c => c.memory.targetEnergySourceId && c.memory.targetEnergySourceNeedsOnlyOneHarvester);
+            if (creepsWorkingSolo && creepsWorkingSolo.length > 0)
+                _.forEach(creepsWorkingSolo, c => RoleCommon._prohibitedIds.push(c.memory.targetEnergySourceId!));
 
-                    if (creep.store.getFreeCapacity() >= closestEnergy.amount) {
-                        creep.memory.targetEnergySourceNeedsOnlyOneHarvester = true;
-                    }
-                }
+            if (!RoleCommon.findStorage(creep)) {
                 // If did not find energy...
-                else {
+                if (!RoleCommon.findDroppedEnery(creep)) {
                     // ...try to find a container
-                    let container: StructureStorage | StructureContainer | null = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                        filter: (structure) => {
-                            return (structure.structureType == STRUCTURE_CONTAINER && structure.store.getUsedCapacity(RESOURCE_ENERGY) > structure.store.getCapacity(RESOURCE_ENERGY) / 2);
-                        }
-                    })
-                    if (container) {
-                        creep.memory.targetEnergySourceId = container.id;
-
-                        if (creep.store.getFreeCapacity() >= container.store.getUsedCapacity()) {
-                            creep.memory.targetEnergySourceNeedsOnlyOneHarvester = true;
-                        }
-                    }
-                    // If did not find a container...
-                    else {
+                    if (!RoleCommon.findContainer(creep)) {
+                        // If did not find a container...
                         // ...try to find an energy source
-                        var source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
-                        if (!source) {
-                            return;
-                        }
-
-                        creep.memory.targetEnergySourceId = source.id;
+                        RoleCommon.FindEnergySource(creep)
                     }
                 }
             }
-            // }
         }
+    }
+
+    private static FindEnergySource(creep: Creep): boolean {
+        var source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+        if (source) {
+            creep.memory.targetEnergySourceId = source.id;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private static findContainer(creep: Creep): boolean {
+        let container: StructureStorage | StructureContainer | null = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_CONTAINER &&
+                        (structure.store.getUsedCapacity(RESOURCE_ENERGY) > structure.store.getCapacity(RESOURCE_ENERGY) / 2) &&
+                        (RoleCommon._prohibitedIds.indexOf(structure.id) == -1));
+            }
+        })
+
+        if (container) {
+            creep.memory.targetEnergySourceId = container.id;
+
+            if (creep.store.getFreeCapacity() >= container.store.getUsedCapacity()) {
+                creep.memory.targetEnergySourceNeedsOnlyOneHarvester = true;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+    private static findStorage(creep: Creep): boolean {
+        let storage: StructureStorage | null = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return ((structure.structureType == STRUCTURE_STORAGE) &&
+                        (structure.store.getUsedCapacity(RESOURCE_ENERGY) > creep.store.getCapacity() &&
+                        RoleCommon._prohibitedIds.indexOf(structure.id) == -1));
+            }
+        })
+
+        if (storage) {
+            creep.memory.targetEnergySourceId = storage.id;
+
+            if (creep.store.getFreeCapacity() >= storage.store.getUsedCapacity()) {
+                creep.memory.targetEnergySourceNeedsOnlyOneHarvester = true;
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private static findDroppedEnery(creep: Creep): boolean {
+        // Find all energies
+        let dropedEnergies = creep.room.find(FIND_DROPPED_RESOURCES);
+        // Find the energies allowed to be picked-up
+        let allowedDropedEnergy = _.filter(dropedEnergies, e => RoleCommon._prohibitedIds.indexOf(e.id) == -1);
+        // Find the closest one
+        let closestEnergy = creep.pos.findClosestByPath(allowedDropedEnergy);
+        // If found something...
+        if (closestEnergy && !creep.memory.forceMoveToTargetContainer) {
+            creep.memory.targetEnergySourceId = closestEnergy.id;
+
+            if (creep.store.getFreeCapacity() >= closestEnergy.amount) {
+                creep.memory.targetEnergySourceNeedsOnlyOneHarvester = true;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private static deleteGetEnergyRelatedMemory(creep: Creep) {
+        delete creep.memory.targetEnergySourceId;
+        delete creep.memory.targetEnergySourceNeedsOnlyOneHarvester;
     }
 }
