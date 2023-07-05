40150644ee3f85efc4b16862fefa6b1c39afe200
Jefferson Rocha <jefferson_adr@hotmail.com>
2023-07-05 07:19:18 +0100

    More progress

--- src/OS/kernel/process-status.ts
+++ src/OS/kernel/process-status.ts
@@ -1,5 +1,5 @@
-export let ProcessStatus = {
-    DEAD: -1,
-    ALIVE: 0,
-    SLEEP: 1,
+export const enum ProcessStatus {
+    DEAD= -1,
+    ALIVE= 0,
+    SLEEP= 1,
 };
--- src/OS/kernel/process.ts
+++ src/OS/kernel/process.ts
@@ -2,93 +2,96 @@ import { ProcessPriority } from "./constants";
 import * as Kernel from "./kernel";
 import { ProcessStatus } from "./process-status";
 
-type ConcreteProcess = { new(pid: number, parentPID: number, priority?: ProcessPriority): Process };
+type ConcreteProcess = { new(pid: number, parentPid: number, priority?: ProcessPriority): Process };
 type DependencyInfo = [ConcreteProcess, ProcessSetupCallback];
 type ProcessSetupCallback = (p: Process) => void;
 
 export abstract class Process {
-    public pid: number;
-    public parentPID: number;
-    public status: number;
-    public abstract classPath(): string;
-    public sleepInfo?: ProcessSleepByTime | ProcessSleepByProcess;
-    public priority: ProcessPriority;
-    public memory: any;
-    protected deps: DependencyInfo[] = [];
-    protected kernel = Kernel;
-    // public static reloadFromTable(pid: number, parentPID: number, priority = ProcessPriority.LowPriority) {
-    //     const p = new Process()
-    // }
-    constructor(pid: number,
-        parentPID: number,
-        priority = ProcessPriority.LowPriority) {
-            this.pid = pid;
-            this.parentPID = parentPID;
-            this.status = ProcessStatus.ALIVE;
-            this.priority = priority;
-    };
+  public status: ProcessStatus;
+  public abstract classPath(): string;
+  public sleepInfo?: ProcessSleepByTime | ProcessSleepByProcess;
+  public memory: any;
+  public deps: DependencyInfo[] = [];
+  public kernel = Kernel;
 
-    public abstract run(): number;
+  constructor(
+    public pid: number,
+    public parentPID: number,
+    public priority = ProcessPriority.LowPriority
+  ) {
+    this.pid = pid;
+    this.parentPID = parentPID;
+    this.status = ProcessStatus.ALIVE;
+    this.priority = priority;
+  }
 
-    public setMemory(memory: any): void {
-        this.memory = memory;
-    };
+  public abstract run(): number;
 
-    public stop(signal: number) {
-        this.kernel.killProcess(this.pid);
-        return signal;
+  public setMemory(memory: any): void {
+    this.memory = memory;
+  }
+
+  public stop(signal: number) {
+    this.kernel.killProcess(this.pid);
+    return signal;
+  }
+
+  public abstract setup(..._: any[]): Process;
+
+  public registerDependency(
+    p: ConcreteProcess,
+    processSetup: ProcessSetupCallback
+  ) {
+    let dependencyInfo: DependencyInfo = [p, processSetup];
+    this.deps.push(dependencyInfo);
+  }
+
+  public runDeps() {
+    let deps = (this.memory.deps = this.memory.deps || {});
+    for (let dep of this.deps) {
+      let [processClass, callback] = dep;
+      let t = new processClass(0, 0, 1);
+      let classPath = t.classPath();
+      if (
+        !deps[classPath] ||
+        !Kernel.getProcessById(deps[classPath])
+      ) {
+        let p = new processClass(0, this.pid);
+        this.kernel.addProcess(p);
+        callback.bind(this)(p);
+        deps[classPath] = p.pid;
+      }
     }
+  }
 
-    public abstract setup(..._: any[]): Process;
+  public suicide(): void {
+    this.kernel.killProcess(this.pid);
+  }
 
-    public registerDependency(p: ConcreteProcess, processSetup: ProcessSetupCallback) {
-        let dependencyInfo: DependencyInfo = [p, processSetup];
-        this.deps.push(dependencyInfo);
-    }
+  public fallAsleepByTime(ticks: number): void {
+    this.kernel.sleepProcessByTime(this, ticks);
+  }
 
-    public runDeps() {
-        let deps = this.memory.deps = this.memory.deps || {};
-        for (let dep of this.deps) {
-            let [processClass, callback] = dep;
-            let t = new processClass(0, 0, 1);
-            let classPath = t.classPath();
-            if ((!deps[classPath]) || (!Kernel.getProcessById(deps[classPath]))) {
-                let p = new processClass(0, this.pid);
-                this.kernel.addProcess(p);
-                callback.bind(this)(p);
-                deps[classPath] = p.pid;
-            }
-        }
-    }
-
-    public suicide(): void {
-        this.kernel.killProcess(this.pid);
-    }
-
-    public fallAsleepByTime(ticks: number): void {
-        this.kernel.sleepProcessByTime(this, ticks);
-    }
-
-    public fallAsleepByProcess(p: Process): void {
-        this.kernel.sleepProcessByProcess(this, p);
-    }
+  public fallAsleepByProcess(p: Process): void {
+    this.kernel.sleepProcessByProcess(this, p);
+  }
 }
 
-const data: any = {}
+const data: any = {};
 export let Lookup = {
-    addProcess: function (p: typeof Process) {
-        data[p.name] = <any>p;
-    },
-    getProcess: function (id: string): any | null {
-        return data[id];
-    }
-}
+  addProcess: function (p: typeof Process) {
+    data[p.name] = p;
+  },
+  getProcess: function (id: string): any | null {
+    return data[id];
+  },
+};
 
 export class ProcessSleepByTime {
-    start: number = 0;
-    duration: number = -1;
+  start: number = 0;
+  duration: number = -1;
 }
 
 export class ProcessSleepByProcess {
-    pid: number = -1;
+  pid: number = -1;
 }
